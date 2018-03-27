fe2860bc2c1e650155282c03adf683656c47348d
Achal B I <achal.rvce@gmail.com>
2018-03-27 22:05:20 +0530

    initial commit

--- test-proj/lambda.js
+++ test-proj/lambda.js
@@ -1,48 +1,44 @@
 let AWS = require('aws-sdk');
 const axios = require('axios');
+var parser = require('xml2json');
 
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
 
-	var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`;
+	var url = `http://www.bhhsall.com/api_buyside/get/office/sort-n/`;
+	var config = {
+		headers: {'ak': '7501F624-02DA-444E-AC44-67E9E9950B3B', 'Content-Type': 'application/json'}
+	};
 
 axios
-	.get(geocodeUrl)
+	.get(url, config)
 	.then(response => {
-		if (response.data.status === 'ZERO_RESULTS') {
+
+		// xml to json
+		var json = parser.toJson(response);
+		console.log("to json -> %s", json);
+
+		if (json.Error) {
 			throw 'result not found';
 		}
-		var lat = response.data.results[0].geometry.location.lat;
-		var lng = response.data.results[0].geometry.location.lng;
-		const APIKey = '7112130bec5122f6adc38125c4483211';
-		var url = `https://api.darksky.net/forecast/${APIKey}/${lat},${lng}`;
-		console.log(response.data.results[0].formatted_address);
-		return axios.get(url);
-	})
-	.then(res => {
-		var temp = res.data.currently.temperature;
-		var summary = res.data.currently.summary;
-		console.log(`the temp is ${temp} , it feels like ${summary}`);
+		// for (office of json.Offices ){
+		// 	ddb.put({
+		// 		TableName: 'integration_offices',
+		// 		Item: { 'vendor_org_id': office.vendor_org_id, 'super_org_id': '102' }
+		// 	}, function (err, data) {
+		// 		if (err) {
+		// 			//handle error
+		// 			console.log(err);
+		// 		} else {
+		// 			//your logic goes here
+		// 			console.log(data);
+		// 		}
+		// 	});
+		// }
+
 	})
 	.catch(err => {
-		if (err.code === 'ENOTFOUND') {
 			console.log(err);
-		} else {
-			console.log(err);
-		}
-	});
-	
-	ddb.put({
-		TableName: 'integration_offices',
-		Item: { 'vendor_org_id': 'NaN', 'super_org_id': '102' }
-	}, function (err, data) {
-		if (err) {
-			//handle error
-			console.log(err);
-		} else {
-			//your logic goes here
-			console.log(data);
-		}
 	});
 
 
