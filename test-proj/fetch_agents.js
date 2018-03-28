a61064db387513e58425bd3bfaf20ce90c73b8b0
Achal B I <achal.rvce@gmail.com>
2018-03-28 13:24:16 +0530

    SNS attr access fix

--- test-proj/fetch_agents.js
+++ test-proj/fetch_agents.js
@@ -1,8 +1,7 @@
 let AWS = require('aws-sdk');
 exports.handler = function(event, context, callback) {
-		console.log(event.Records[0].Sns.MessageAttributes);
-var officeId = event.Records[0].Sns.MessageAttributes.office_id.StringValue;
-	console.log(officeId)
+var officeId = event.Records[0].Sns.MessageAttributes.office_id.Value;
+	console.log("Office id: ",officeId)
 
 	callback(null,'Successfully executed fetch agent');
 }
